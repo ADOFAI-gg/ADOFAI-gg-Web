9cad6383b408fc3d0ca5208457de1873bff70380
DESKTOP-L6M8H8B\alvin <alvinkang1@gmail.com>
2021-07-08 20:29:36 +0900

    Feat: Add Main Additional Information

--- src/App.js
+++ src/App.js
@@ -19,8 +19,26 @@ function App() {
       </div>
       <div className="main">
         <img className="main-logo" src="http://localhost:3000/logo.png" alt="" />
-        <h2 style={{ marginTop: '10px' }}>Based On The <a href="https://docs.google.com/spreadsheets/d/1PzLHfWmVWJHrBGnNSsLTsdH0ibdk0hB4MpKHET1nkpU/edit#gid=1848316468" target="_blank"><span style={{ color: '#FFE76E', fontWeight: '700' }}>Unofficial ADOFAI Forum</span></a></h2>
+        <h2 style={{ marginTop: '10px' }}>Based On The <a href="https://docs.google.com/spreadsheets/d/1PzLHfWmVWJHrBGnNSsLTsdH0ibdk0hB4MpKHET1nkpU/edit#gid=1848316468" target="_blank"><span className="yellow-highlight">Unofficial ADOFAI Forum</span></a></h2>
         <input className="main-search-bar" type="text" placeholder="Search Song, Artist, or Creator" />
+        <div className="main-additional-information">
+          <table>
+            <tr><h3>Players Online</h3></tr>
+            <tr><h2><span className="yellow-highlight">999999</span></h2></tr>
+          </table>
+          <table>
+            <tr><h3>Ranked Players</h3></tr>
+            <tr><h2><span className="yellow-highlight">999999</span></h2></tr>
+          </table>
+          <table>
+            <tr><h3>Ranked Maps</h3></tr>
+            <tr><h2><span className="yellow-highlight">999999</span></h2></tr>
+          </table>
+          <table>
+            <tr><h3>Uncleared Maps</h3></tr>
+            <tr><h2><span className="yellow-highlight">999999</span></h2></tr>
+          </table>
+        </div>
       </div>
     </div>
   );
