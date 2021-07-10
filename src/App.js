8c603f10c420d0deb81e59bdd0b70a63a305bcdd
DESKTOP-L6M8H8B\alvin <alvinkang1@gmail.com>
2021-07-10 18:04:40 +0900

    Feat: Finish Popular Maps - Finished popular maps section, changed icons, no text drag

--- src/App.js
+++ src/App.js
@@ -40,28 +40,58 @@ function App() {
           </table>
         </div>
         <div className="main-popular-maps-title">
-          {/* <div style={{ flexBasis: '80%', textAlign: 'left' }}><h1>Recent Popular Maps</h1></div> */}
-          {/* <div style={{ flexBasis: '20%', textAlign: 'right' }}><h3><a style={{ color: 'white' }} href="#maps">See All ▹</a></h3></div> */}
           <h1 style={{ flexBasis: '80%', textAlign: 'left' }}>Recent Popular Maps</h1>
-          <h3 style={{ flexBasis: '20%', textAlign: 'right', paddingTop: '20px' }}><a style={{ color: 'white' }} href="#maps">See All ▹</a></h3>
+          <h3 style={{ flexBasis: '20%', textAlign: 'right', paddingTop: '20px' }}><a href="#maps">See All ▹</a></h3>
         </div>
         <div className="main-popular-maps">
           <div className="map-info-container">
-            <img style={{ marginRight: '20px' }} src="http://localhost:3000/level.png" alt="" />
-            <table border="0" cellSpacing="0" cellPadding="0" width="300" style={{ textAlign: "left" }}>
+            <img src="http://localhost:3000/level.png" alt="" />
+            <table border="0" cellSpacing="0" cellPadding="0" width="340" style={{ textAlign: "left", marginLeft: "10px" }}>
               <tr>
                 <td className="map-info-top">Song</td>
               </tr>
               <tr>
-                <td valign="bottom" className="map-info-bottom">I love girrafes</td>
+                <td valign="bottom" className="map-info-song"><a href="">A very very very very very very long title</a></td>
               </tr>  
             </table>
-            <table border="0" cellSpacing="0" cellPadding="0" width="200" style={{ textAlign: "left" }}>
+            <table border="0" cellSpacing="0" cellPadding="0" width="200" style={{ textAlign: "left", marginLeft: "10px" }}>
               <tr>
                 <td className="map-info-top">Artist</td>
               </tr>
               <tr>
-                <td valign="bottom" className="map-info-bottom" style={{ maxWidth: "200px" }}>かめりあ(Camellia)</td>
+                <td valign="bottom" className="map-info-artist"><a href="">A very very long artist</a></td>
+              </tr>  
+            </table>
+            <table border="0" cellSpacing="0" cellPadding="0" width="200" style={{ textAlign: "left", marginLeft: "10px" }}>
+              <tr>
+                <td className="map-info-top">Creator</td>
+              </tr>
+              <tr>
+                <td valign="bottom" className="map-info-creator"><a href="">A very very long creator</a></td>
+              </tr>  
+            </table>
+            <table border="0" cellSpacing="0" cellPadding="0" width="110" style={{ textAlign: "center", marginLeft: "10px" }}>
+              <tr>
+                <td className="map-info-top">BPM</td>
+              </tr>
+              <tr>
+                <td valign="bottom" className="map-info-bpm">999</td>
+              </tr>  
+            </table>
+            <table border="0" cellSpacing="0" cellPadding="0" width="70" style={{ textAlign: "center", marginLeft: "10px" }}>
+              <tr>
+                <td className="map-info-top">Tiles</td>
+              </tr>
+              <tr>
+                <td valign="bottom" className="map-info-tiles">9999</td>
+              </tr>  
+            </table>
+            <table border="0" cellSpacing="0" cellPadding="0" width="70" style={{ textAlign: "center", marginLeft: "10px" }}>
+              <tr>
+                <td valign="bottom" className="map-info-top"><img style={{ height: '12px' }} src="http://localhost:3000/heart.png" alt="" /></td>
+              </tr>
+              <tr>
+                <td valign="bottom" className="map-info-likes">9999</td>
               </tr>  
             </table>
           </div>
