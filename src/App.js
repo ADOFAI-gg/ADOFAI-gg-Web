47c0f0654092ec14d57c918655dde3f8e3296a8e
DESKTOP-L6M8H8B\alvin <alvinkang1@gmail.com>
2021-07-14 19:56:09 +0900

    Style: Convert Main Additional Information To Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar'
+import MainAddInfo from './components/MainAddInfo';
 
 function App() {
   return (
@@ -8,24 +9,7 @@ function App() {
         <img className="main-logo" src="http://localhost:3000/logo.svg" alt="" />
         <h2 style={{ marginTop: '10px' }}>Based On The <a href="https://docs.google.com/spreadsheets/d/1PzLHfWmVWJHrBGnNSsLTsdH0ibdk0hB4MpKHET1nkpU/edit#gid=1848316468" target="_blank"><span className="yellow-highlight">Unofficial ADOFAI Forum</span></a></h2>
         <input className="main-search-bar" type="text" placeholder="Search Song, Artist, or Creator" />
-        <div className="main-additional-information">
-          <table>
-            <tr><h3>Players Online</h3></tr>
-            <tr><h2><span className="yellow-highlight">999999</span></h2></tr>
-          </table>
-          <table>
-            <tr><h3>Ranked Players</h3></tr>
-            <tr><h2><span className="yellow-highlight">999999</span></h2></tr>
-          </table>
-          <table>
-            <tr><h3>Ranked Maps</h3></tr>
-            <tr><h2><span className="yellow-highlight">999999</span></h2></tr>
-          </table>
-          <table>
-            <tr><h3>Uncleared Maps</h3></tr>
-            <tr><h2><span className="yellow-highlight">999999</span></h2></tr>
-          </table>
-        </div>
+        <MainAddInfo playersOnline='999999' rankedPlayers='999999' rankedMaps='999999' unclearedMaps='999999'/>
         <div className="content-title">
           <h1 style={{ flexBasis: '80%', textAlign: 'left' }}>Recent Top Plays</h1>
           <h3 style={{ flexBasis: '20%', textAlign: 'right', paddingTop: '20px' }}><a href="#maps">See All ▹</a></h3>
