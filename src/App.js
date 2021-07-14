623fcef46a569968d02b900343c44379eebf3e3d
DESKTOP-L6M8H8B\alvin <alvinkang1@gmail.com>
2021-07-14 19:22:40 +0900

    Style: Convert Navbar To Component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
+import Navbar from './components/Navbar'
+
 function App() {
   return (
     <div className="container">
-      <div className="navbar">
-        <div className="navbar-logo">
-          <a href="#home">
-            <img className="logo-middle" style={{ height: '20px', marginLeft: '10px' }} src="http://localhost:3000/logo.svg" alt="" />
-          </a>
-        </div>
-        <div className="navbar-content">
-          <ul>
-            <li><a href="#maps">Maps</a></li>
-            <li><a href="#mods">Mods</a></li>
-            <li><a href="#ranking">Ranking</a></li>
-          </ul>
-        </div>
-        <div className="navbar-login"></div>
-      </div>
+      <Navbar />
       <div className="main">
         <img className="main-logo" src="http://localhost:3000/logo.svg" alt="" />
         <h2 style={{ marginTop: '10px' }}>Based On The <a href="https://docs.google.com/spreadsheets/d/1PzLHfWmVWJHrBGnNSsLTsdH0ibdk0hB4MpKHET1nkpU/edit#gid=1848316468" target="_blank"><span className="yellow-highlight">Unofficial ADOFAI Forum</span></a></h2>
