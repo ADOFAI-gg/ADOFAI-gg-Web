39401ed114a247476c00e42d3d62faae02cfc968
DESKTOP-L6M8H8B\alvin <alvinkang1@gmail.com>
2021-07-18 02:09:24 +0900

    Feat: Add Additional Map Information - Fixed recent top plays to fit in one div, changed map info background to blurred image, hover on map info now reveals additional information

--- src/App.js
+++ src/App.js
@@ -21,57 +21,98 @@ function App() {
           <h3 style={{ flexBasis: '20%', textAlign: 'right', paddingTop: '20px' }}><a href="#maps">See All ▹</a></h3>
         </div>
         <div className="main-popular-maps">
-          <div className="map-info-container">
-            <img style={{ marginLeft: '10px' }} src="http://localhost:3000/level.png" alt="" />
-            <table border="0" cellSpacing="0" cellPadding="0" width="320" style={{ textAlign: "left", marginLeft: "10px" }}>
-              <tr>
-                <td className="map-info-top">Song</td>
-              </tr>
-              <tr>
-                <td valign="bottom" className="map-info-song"><a href="">A very very very very very long title</a></td>
-              </tr>  
-            </table>
-            <table border="0" cellSpacing="0" cellPadding="0" width="200" style={{ textAlign: "left", marginLeft: "10px" }}>
-              <tr>
-                <td className="map-info-top">Artist</td>
-              </tr>
-              <tr>
-                <td valign="bottom" className="map-info-artist"><a href="">A very very long artist</a></td>
-              </tr>  
-            </table>
-            <table border="0" cellSpacing="0" cellPadding="0" width="200" style={{ textAlign: "left", marginLeft: "10px" }}>
-              <tr>
-                <td className="map-info-top">Creator</td>
-              </tr>
-              <tr>
-                <td valign="bottom" className="map-info-creator"><a href="">A very very long creator</a></td>
-              </tr>  
-            </table>
-            <table border="0" cellSpacing="0" cellPadding="0" width="110" style={{ textAlign: "center", marginLeft: "10px" }}>
-              <tr>
-                <td className="map-info-top">BPM</td>
-              </tr>
-              <tr>
-                <td valign="bottom" className="map-info-bpm">999</td>
-              </tr>  
-            </table>
-            <table border="0" cellSpacing="0" cellPadding="0" width="70" style={{ textAlign: "center", marginLeft: "10px" }}>
-              <tr>
-                <td className="map-info-top">Tiles</td>
-              </tr>
-              <tr>
-                <td valign="bottom" className="map-info-tiles">9999</td>
-              </tr>  
-            </table>
-            <table border="0" cellSpacing="0" cellPadding="0" width="70" style={{ textAlign: "center", marginLeft: "10px" }}>
-              <tr>
-                <td valign="bottom" className="map-info-top"><img style={{ height: '12px' }} src="http://localhost:3000/heart.svg" alt="" /></td>
-              </tr>
-              <tr>
-                <td valign="bottom" className="map-info-likes">9999</td>
-              </tr>  
-            </table>
-          </div>
+          <a href="#map">
+            <div className="map-info">
+              <div className="map-info-container-bottom">
+                <img style={{ marginLeft: '10px' }} src="http://localhost:3000/level_icons/18.png" alt="" />
+                <table border="0" cellSpacing="0" cellPadding="0" width="400" style={{ textAlign: "left", marginLeft: "10px" }}>
+                  <tr>
+                    <td className="map-info-top">Map</td>
+                  </tr>
+                  <tr>
+                    <td valign="bottom" className="map-info-map">A very very very very long map title</td>
+                  </tr>  
+                </table>
+                <table border="0" cellSpacing="0" cellPadding="0" width="200" style={{ textAlign: "left", marginLeft: "10px" }}>
+                  <tr>
+                    <td className="map-info-top">Creator</td>
+                  </tr>
+                  <tr>
+                    <td valign="bottom" className="map-info-creator">A very very long creator</td>
+                  </tr>  
+                </table>
+                <table border="0" cellSpacing="0" cellPadding="0" width="130" style={{ textAlign: "center", marginLeft: "10px" }}>
+                  <tr>
+                    <td className="map-info-top">BPM</td>
+                  </tr>
+                  <tr>
+                    <td valign="bottom" className="map-info-bpm">9999-9999</td>
+                  </tr>  
+                </table>
+                <table border="0" cellSpacing="0" cellPadding="0" width="80" style={{ textAlign: "center", marginLeft: "10px" }}>
+                  <tr>
+                    <td className="map-info-top">Tiles</td>
+                  </tr>
+                  <tr>
+                    <td valign="bottom" className="map-info-tiles">9999+</td>
+                  </tr>  
+                </table>
+                <table border="0" cellSpacing="0" cellPadding="0" width="80" style={{ textAlign: "center", marginLeft: "10px" }}>
+                  <tr>
+                    <td valign="bottom" className="map-info-top"><img style={{ height: '12px' }} src="http://localhost:3000/comment.svg" alt="" /></td>
+                  </tr>
+                  <tr>
+                    <td valign="bottom" className="map-info-comments">9999+</td>
+                  </tr>  
+                </table>
+                <table border="0" cellSpacing="0" cellPadding="0" width="80" style={{ textAlign: "center", marginLeft: "10px" }}>
+                  <tr>
+                    <td valign="bottom" className="map-info-top"><img style={{ height: '12px' }} src="http://localhost:3000/heart.svg" alt="" /></td>
+                  </tr>
+                  <tr>
+                    <td valign="bottom" className="map-info-likes">9999+</td>
+                  </tr>  
+                </table>
+              </div>
+              <div className="map-info-container-top">
+                <img style={{ marginLeft: '10px' }} src="http://localhost:3000/level_icons/18.png" alt="" />
+                <table border="0" cellSpacing="0" cellPadding="0" width="400" style={{ textAlign: "left", marginLeft: "10px" }}>
+                  <tr>
+                    <td className="map-info-top">Song</td>
+                  </tr>
+                  <tr>
+                    <td valign="bottom" className="map-info-song">A very very very very long song title</td>
+                  </tr>  
+                </table>
+                <table border="0" cellSpacing="0" cellPadding="0" width="250" style={{ textAlign: "left", marginLeft: "10px" }}>
+                  <tr>
+                    <td className="map-info-top">Artist</td>
+                  </tr>
+                  <tr>
+                    <td valign="bottom" className="map-info-artist">A very very long artist</td>
+                  </tr>  
+                </table>
+                <table border="0" cellSpacing="0" cellPadding="0" width="335" style={{ textAlign: "left", marginLeft: "10px" }}>
+                  <tr>
+                    <td className="map-info-top">Tags</td>
+                  </tr>
+                  <tr>
+                    <td valign="bottom" className="map-info-tags">
+                      <img style={{ height:'20px', marginTop:'8px', marginRight:'5px'}} src="http://localhost:3000/updown.svg" alt="" />
+                      <img style={{ height:'20px', marginTop:'8px', marginRight:'5px'}} src="http://localhost:3000/updown.svg" alt="" />
+                      <img style={{ height:'20px', marginTop:'8px', marginRight:'5px'}} src="http://localhost:3000/updown.svg" alt="" />
+                      <img style={{ height:'20px', marginTop:'8px', marginRight:'5px'}} src="http://localhost:3000/updown.svg" alt="" />
+                      <img style={{ height:'20px', marginTop:'8px', marginRight:'5px'}} src="http://localhost:3000/updown.svg" alt="" />
+                      <img style={{ height:'20px', marginTop:'8px', marginRight:'5px'}} src="http://localhost:3000/updown.svg" alt="" />
+                      <img style={{ height:'20px', marginTop:'8px', marginRight:'5px'}} src="http://localhost:3000/updown.svg" alt="" />
+                      <img style={{ height:'20px', marginTop:'8px', marginRight:'5px'}} src="http://localhost:3000/updown.svg" alt="" />
+                      <img style={{ height:'20px', marginTop:'8px', marginRight:'5px'}} src="http://localhost:3000/add.svg" alt="" />
+                    </td>
+                  </tr>  
+                </table>
+              </div>
+            </div>
+          </a>
         </div>
       </div>
       <div className="footer">
