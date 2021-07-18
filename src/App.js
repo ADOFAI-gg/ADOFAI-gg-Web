299151a6d5e52d2a02cc84b6c4aa3053815b61d3
DESKTOP-L6M8H8B\alvin <alvinkang1@gmail.com>
2021-07-18 19:19:29 +0900

    Style: Convert Footer To Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import MainAddInfo from './components/MainAddInfo';
 import MainTopPlays from './components/MainTopPlays';
 import MainPopularMaps from './components/MainPopularMaps';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -15,11 +16,7 @@ function App() {
         <MainTopPlays />
         <MainPopularMaps />
       </div>
-      <div className="footer">
-        <div className="footer-content">
-          <h4 style={{ fontWeight: '400' }}>ADOFAI.GG is not associated with <a href="https://7thbe.at/" target="_blank">7th Beat Games</a>.&ensp;|&ensp;<a href="https://discord.gg/rwWyfPJ" target="_blank">Join Our Discord</a>&ensp;|&ensp;<a href="mailto:adofai.gg@gmail.com">Contact Us</a></h4>
-        </div>
-      </div>
+      <Footer />
     </div>
   );
 }
