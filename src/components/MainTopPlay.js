f9e168647d9f33114e21b108e69a834221692ebc
ppapman1 <mail.bravery.0001@gmail.com>
2021-07-19 12:31:27 +0900

    Chore: Replace 'levelLevel' with 'levelDifficulty'

--- src/components/MainTopPlay.js
+++ src/components/MainTopPlay.js
@@ -4,7 +4,7 @@ const MainTopPlay = ({ recentTopPlay }) => {
     <div className="main-top-plays-container">
       <img className="main-top-plays-image" src={'https://i3.ytimg.com/vi/' + recentTopPlay.playVideo.split('=')[1] + '/0.jpg'} alt="" />
       <div className="main-top-plays-title"><a href={'/levels/' + recentTopPlay.levelId}>{recentTopPlay.levelTitle}</a></div>
-      <img className="main-top-plays-level" src={'http://localhost:3000/level_icons/' + recentTopPlay.levelLevel + '.png'} alt="" />
+      <img className="main-top-plays-level" src={'http://localhost:3000/level_icons/' + recentTopPlay.levelDifficulty + '.png'} alt="" />
       <div className="main-top-plays-subtitle">{recentTopPlay.levelSubTitle}</div>
       <div className="main-top-plays-player">{recentTopPlay.playerName}</div>
       <div className="main-top-plays-speed">Speed: <span className="yellow-highlight">{recentTopPlay.playPitch + '%'}</span></div>
