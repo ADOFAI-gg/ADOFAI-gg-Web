18f05e3279968de6a6431740e0ad5e733bdd67db
ppapman1 <mail.bravery.0001@gmail.com>
2021-07-24 23:43:09 +0900

    Fix: Fix class name

--- src/components/MainAddInfo.js
+++ src/components/MainAddInfo.js
@@ -3,19 +3,19 @@ const MainAddInfo = ({ playersOnline, rankedPlayers, rankedLevels, unclearedLeve
     <div className="main-additional-information">
     <table>
       <tr><h3>Players Online</h3></tr>
-      <tr><h2><span className="yellow-highlight">{playersOnline}</span></h2></tr>
+      <tr><h2><span className="main-yellow-highlight">{playersOnline}</span></h2></tr>
     </table>
     <table>
       <tr><h3>Ranked Players</h3></tr>
-      <tr><h2><span className="yellow-highlight">{rankedPlayers}</span></h2></tr>
+      <tr><h2><span className="main-yellow-highlight">{rankedPlayers}</span></h2></tr>
     </table>
     <table>
       <tr><h3>Ranked Levels</h3></tr>
-      <tr><h2><span className="yellow-highlight">{rankedLevels}</span></h2></tr>
+      <tr><h2><span className="main-yellow-highlight">{rankedLevels}</span></h2></tr>
     </table>
     <table>
       <tr><h3>Uncleared Levels</h3></tr>
-      <tr><h2><span className="yellow-highlight">{unclearedLevels}</span></h2></tr>
+      <tr><h2><span className="main-yellow-highlight">{unclearedLevels}</span></h2></tr>
     </table>
     </div>
   );
