50bfa9c9051e38631b7050c578de32db782c7c23
ppapman1 <mail.bravery.0001@gmail.com>
2021-08-14 23:43:37 +0900

    Feat: Add RackingPage route, Clean up Router code

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import HomePage from '../routes/HomePage';
 import LevelPage from '../routes/LevelPage';
 import ModListPage from '../routes/ModListPage';
@@ -10,28 +11,14 @@ const AppRouter = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/levels">
-          <LevelListPage />
-        </Route>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/levels" component={LevelListPage} />
+        <Route path="/levels/:id" component={LevelPage} />
+        <Route path="/mods" component={ModListPage} />
+        <Route path="/mods/:id" component={ModPage} />
 
-        <Route exact path="/levels/:id">
-          <LevelPage />
-        </Route>
-
-        <Route exact path="/mods">
-          <ModListPage />
-        </Route>
-
-        <Route exact path="/mods/:id">
-          <ModPage />
-        </Route>
-
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-
-        {/* 404 NOT FOUND PAGE */}
         <Route>
+          {/* XXX to component */}
           <main>
             <br />
             <h2>Oops! This page does not exist.</h2>
---
+++ src/routes/RankingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RankingPage = () => {
+  return (
+    <div>
+      <h2>RankingPage</h2>
+    </div>
+  );
+};
+
+export default RankingPage;
