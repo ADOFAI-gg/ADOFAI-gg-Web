bdb0521f279b7d9c432af993de0c70776dc6aeec
ppapman <48434316+ppapman1@users.noreply.github.com>
2021-10-20 01:35:26 +0900

    Test: Test infinite scroll performane
    Co-authored-by: Rhythmfactory <jh031833@gmail.com>

--- src/routes/TestPage.js
+++ src/routes/TestPage.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import InfiniteLoading from "react-simple-infinite-loading";
 
 const TestPage = () => {
-  return <main>this is test page.</main>;
+  const [items, setItems] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+
+  const fetchMore = () => {
+    setTimeout(() => {
+      setItems(
+        items.concat(Array.from({ length: 25 }).map((_) => Math.random()))
+      );
+
+      setHasMore(items.length < 100000);
+    }, 0);
+  };
+
+  return (
+    <main>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <InfiniteLoading
+          hasMoreItems={hasMore}
+          itemHeight={84}
+          loadMoreItems={fetchMore}
+        >
+          {items.map((item, index) => (
+            <div
+              key={item}
+              style={{
+                padding: "30px",
+                fontSize: "1.2em",
+                display: "flex",
+                gap: "10px",
+                alignItems: "center",
+              }}
+            >
+              <img
+                className="level-item-info-difficulty"
+                src={`difficulty_icons/${Math.floor(item * 22)}.svg`}
+                alt={`Lv. ${Math.floor(item * 22)}`}
+              />
+              Ah my favorite level, #{index}!
+            </div>
+          ))}
+        </InfiniteLoading>
+      </div>
+    </main>
+  );
 };
 
 export default TestPage;
