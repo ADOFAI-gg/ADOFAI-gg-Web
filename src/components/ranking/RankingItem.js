403a6dffe11aa9821976e86e42c57f3272ec69c5
ppapman <48434316+ppapman1@users.noreply.github.com>
2021-10-29 11:56:55 +0900

    Chore(RankingItem): Edit layout of highest info

--- src/components/ranking/RankingItem.js
+++ src/components/ranking/RankingItem.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const RankingItem = ({ rank, index }) => {
+  const speed = String(rank.bestPlay.speed / 100);
+
   return (
     <article className="ranking-item">
       <div className="ranking-item-rank">#{index + 1}</div>
@@ -14,7 +16,6 @@ const RankingItem = ({ rank, index }) => {
           </div>
 
           <div className="ranking-item-detail-highest notranslate">
-            {/* href={`/levels/${rank.bestPlay.id}`} */}
             <Link
               to={`/levels/${rank.bestPlay.levelId}`}
               className="ranking-item-detail-highest-title"
@@ -25,34 +26,36 @@ const RankingItem = ({ rank, index }) => {
               {" - "}
               {rank.bestPlay.title}
             </Link>
+
             <span className="ranking-item-detail-highest-info">
-              <div>
+              <div className="ranking-item-detail-highest-info-speed">
                 <img
                   src={"other_icons/speed.svg"}
                   alt="Speed Trial: "
                   style={{ height: "0.9em", marginRight: "3px" }}
                 />
-                {rank.bestPlay.speed / 100}x
+                {speed.startsWith("0.") ? speed.replace("0", "") : speed}x
               </div>
-              {rank.bestPlay.rawAccuracy && (
-                <div>
-                  <img
-                    src={"other_icons/accuracy.svg"}
-                    alt="Accurancy: "
-                    style={{
-                      height: "0.9em",
-                      marginRight: "3px",
-                    }}
-                  />
-                  {rank.bestPlay.rawAccuracy.toFixed(1)}%
-                </div>
-              )}
-              <div>
-                (Lv.{" "}
+
+              <div className="ranking-item-detail-highest-info-accuracy">
+                <img
+                  src={"other_icons/accuracy.svg"}
+                  alt="Accurancy: "
+                  style={{
+                    height: "0.9em",
+                    marginRight: "3px",
+                  }}
+                />
+                {rank.bestPlay.rawAccuracy
+                  ? `${rank.bestPlay.rawAccuracy.toFixed(1)}%`
+                  : `///./%`}
+              </div>
+
+              <div className="ranking-item-detail-highest-info-difficulty">
+                Lv.
                 {rank.bestPlay.difficulty < 20
                   ? String(rank.bestPlay.difficulty).replace(".5", "+")
                   : rank.bestPlay.difficulty}
-                )
               </div>
             </span>
           </div>
