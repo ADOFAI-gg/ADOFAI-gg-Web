3348588248dadbb3b4c278292d386ad05679c107
ppapman <mail.bravery.0001@gmail.com>
2022-02-04 19:28:11 +0900

    style: style global.css, main.css, re-style footer

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -4,26 +4,24 @@ const Footer = () => {
   return (
     <footer>
       <div className='footer-content'>
-        <h4 style={{ fontWeight: '400' }}>
-          ADOFAI.GG is not associated with{' '}
-          <a href='https://7thbe.at/' target='_blank' rel='noreferrer'>
-            7th Beat Games
-          </a>
-          {'. | '}
-          <a
-            href='https://discord.gg/TKdpbUUfUa'
-            target='_blank'
-            rel='noreferrer'
-          >
-            Join Our Discord
-          </a>
-          {' | '}
-          <a href='mailto:adofai.gg@gmail.com'>Contact Us</a>
-          {' | Powered By '}
-          <a href='https://www.hanmesoft.com/' target='_blank' rel='noreferrer'>
-            Hanmesoft
-          </a>
-        </h4>
+        ADOFAI.GG is not associated with{' '}
+        <a href='https://7thbe.at/' target='_blank' rel='noreferrer'>
+          7th Beat Games
+        </a>
+        {'. | '}
+        <a
+          href='https://discord.gg/TKdpbUUfUa'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Join Our Discord
+        </a>
+        {' | '}
+        <a href='mailto:adofai.gg@gmail.com'>Contact Us</a>
+        {' | Powered By '}
+        <a href='https://www.hanmesoft.com/' target='_blank' rel='noreferrer'>
+          Hanmesoft
+        </a>
       </div>
     </footer>
   );
