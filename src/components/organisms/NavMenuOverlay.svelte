459d595b36fc2357a3163849253ab7ef874a9d23
pikokr <parangee9706@gmail.com>
2022-05-16 23:34:44 +0900

    fix: duplicate `display` utility class

--- src/components/organisms/NavMenuOverlay.svelte
+++ src/components/organisms/NavMenuOverlay.svelte
@@ -45,7 +45,7 @@
     }
   )}
 >
-  <div class="block md:hidden flex flex-col gap-4">
+  <div class="md:hidden flex flex-col gap-4">
     <NavLinks />
     <div class="bg-white bg-opacity-20 w-full h-[1px]" />
     <NavSignArea />
