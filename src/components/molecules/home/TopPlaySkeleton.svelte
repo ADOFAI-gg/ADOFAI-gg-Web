4a5aa266c2171fa978cebb2478247af958afa46a
pikokr <parangee9706@gmail.com>
2022-05-16 21:26:53 +0900

    feat: responsive top plays section

--- src/components/molecules/home/TopPlaySkeleton.svelte
+++ src/components/molecules/home/TopPlaySkeleton.svelte
@@ -1,4 +1,6 @@
-<div class="flex flex-col gap-[15px] items-center opacity-40">
+<div
+  class="flex flex-col gap-[15px] items-center opacity-40 last:md:col-span-2 last:md:px-[25%] last:lg:p-0 last:lg:col-span-1"
+>
   <div class="w-full aspect-video bg-white bg-repeat-round animate-pulse rounded-[5px]" />
   <div class="flex flex-col gap-[4px] w-full items-center">
     <div class="bg-white w-[30%] h-8 rounded-[5px] animate-pulse" />
--- src/components/organisms/home/TopPlaysSection.svelte
+++ src/components/organisms/home/TopPlaysSection.svelte
@@ -8,7 +8,7 @@
 
 <MainSectionTitle title="Top Plays" moreLink="/ranking" />
 
-<div class="grid grid-cols-3 w-full gap-[50px]">
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full gap-[50px]">
   {#if plays.length}
     hello
   {:else}
