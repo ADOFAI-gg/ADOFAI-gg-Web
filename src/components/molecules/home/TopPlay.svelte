dd33b7fa4e348c15d280389e97ed2f426a1f7aad
pikokr <parangee9706@gmail.com>
2022-05-19 22:57:41 +0900

    feat: top play details

--- src/components/molecules/home/TopPlay.svelte
+++ src/components/molecules/home/TopPlay.svelte
@@ -5,6 +5,7 @@
   import Image from '@/components/atoms/Image.svelte';
   import type { PlayLog } from '@/types';
   import { getYoutubeVideoId } from '@/utils/youtube';
+  import TopPlayDetailPair from './TopPlayDetailPair.svelte';
 
   export let play: PlayLog;
 
@@ -14,24 +15,39 @@
 </script>
 
 <div
-  class="flex flex-col gap-[15px] items-center last:md:col-span-2 last:md:px-[25%] last:lg:p-0 last:lg:col-span-1"
+  class="lg:first:mx-0 lg:last:ml-auto md:last:mr-0 last:md:col-span-2 max-w-[360px] md:max-w-none lg:max-w-[360px] mx-auto last:lg:col-span-1 last:md:px-[25%] last:lg:p-0"
 >
-  <div class="w-full aspect-video bg-repeat-round rounded-[5px] overflow-hidden">
-    <Image
-      width="100%"
-      height="100%"
-      src="https://i.ytimg.com/vi/{getYoutubeVideoId(play.url)}/original.jpg"
-    />
-  </div>
-  <div class="flex flex-col gap-[4px] w-full items-center">
-    <div class="text-2xl font-bold">
-      {play.player.name}
+  <a
+    href={play.url}
+    rel="noreferrer"
+    target="_blank"
+    class="flex  flex-col gap-[15px] items-center"
+  >
+    <div class="w-full aspect-video bg-repeat-round rounded-[5px] overflow-hidden">
+      <Image
+        width="100%"
+        height="100%"
+        src="https://i.ytimg.com/vi/{getYoutubeVideoId(play.url)}/original.jpg"
+      />
     </div>
-    <div class="flex gap-[8px] items-center">
-      {#await difficulty then difficulty}
-        <DifficultyIcon {difficulty} />
-      {/await}
-      <div class="font-medium">{play.level.name}</div>
+    <div class="flex flex-col gap-[4px] w-full items-center">
+      <div class="text-2xl font-bold">
+        {play.player.name}
+      </div>
+      <div class="flex gap-[8px] items-center">
+        {#await difficulty then difficulty}
+          <DifficultyIcon {difficulty} />
+        {/await}
+        <div class="font-medium">{play.level.name}</div>
+      </div>
+      <div class="flex gap-[24px]">
+        <TopPlayDetailPair icon="accuracy" value="{play.accuracy}%" />
+        <TopPlayDetailPair
+          icon="speed"
+          value="{(play.speed / 100).toLocaleString('en', { minimumFractionDigits: 1 })}X"
+        />
+        <TopPlayDetailPair icon="fire" value="{play.playPoint.toFixed(0)} PP" />
+      </div>
     </div>
-  </div>
+  </a>
 </div>
---
+++ src/components/molecules/home/TopPlayDetailPair.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Icon from '@/components/atoms/Icon.svelte';
+
+  export let icon: string;
+  export let value: string;
+</script>
+
+<div class="flex gap-[6px] opacity-60">
+  <Icon {icon} size={16} />
+  <span class="text-md">{value}</span>
+</div>
