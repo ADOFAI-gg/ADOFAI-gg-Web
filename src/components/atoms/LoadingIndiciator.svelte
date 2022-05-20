13608a53140ac54c0b83bc7e07f2feec5e649791
pikokr <parangee9706@gmail.com>
2022-05-20 21:13:21 +0900

    feat: page loading indicator

---
+++ src/components/atoms/LoadingIndiciator.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { navigating } from '$app/stores';
+  import LoadingSpinner from '@/components/atoms/LoadingSpinner.svelte';
+  import { fade } from 'svelte/transition';
+</script>
+
+{#if $navigating}
+  <div class="fixed right-[10px] top-[10px] z-[1000]" transition:fade>
+    <LoadingSpinner filled size={36} />
+  </div>
+{/if}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,23 @@
+<script context="module" lang="ts">
+  import type { Load } from '@sveltejs/kit';
+  export const load: Load = async ({ url }) => ({ props: { url } });
+</script>
+
 <script lang="ts">
   import '../stylesheets/main.scss';
   import Nav from '../components/organisms/Nav.svelte';
+  import LoadingIndiciator from '@/components/atoms/LoadingIndiciator.svelte';
+  export let url: string;
+  import { fade } from 'svelte/transition';
+
+  $: console.log(url);
 </script>
 
+<LoadingIndiciator />
+
 <Nav />
-<slot />
+{#key url}
+  <div in:fade={{ duration: 250, delay: 300 }} out:fade={{ duration: 250 }}>
+    <slot />
+  </div>
+{/key}
