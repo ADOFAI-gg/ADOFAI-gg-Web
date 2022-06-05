b218e6ebfef38f5c214c3e7478bd4ea869b65f6f
pikokr <parangee9706@gmail.com>
2022-06-05 12:15:25 +0900

    fix: long tag id

--- src/components/organisms/levels/MusicTagSearchArea.svelte
+++ src/components/organisms/levels/MusicTagSearchArea.svelte
@@ -8,6 +8,6 @@
 <SearchTagGroupBase title="TAG_GROUP_MUSIC">
   <MusicLengthSelectItem tag={1} value="short" />
   <MusicLengthSelectItem icon="lengthMedium" namespace="MUSIC" tag="MEDIUM" value="medium" />
-  <MusicLengthSelectItem tag={1} value="long" />
+  <MusicLengthSelectItem tag={11} value="long" />
   <TagCheckbox tag={4} />
 </SearchTagGroupBase>
