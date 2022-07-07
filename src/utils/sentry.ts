5eefd2e8b3af20be9432ce4801c6bcc648575c27
pikokr <parangee9706@gmail.com>
2022-07-07 17:06:14 +0900

    feat: sentry rrweb integration

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,7 +1,9 @@
 import * as Sentry from '@sentry/browser';
+import SentryRRWeb from '@sentry/rrweb';
 
 export const setupSentry = () => {
   Sentry.init({
-    dsn: 'https://2ecc5abf3f814edd8a769cb770f26dde@sentry.oci.pikokr.dev/3'
+    dsn: 'https://2ecc5abf3f814edd8a769cb770f26dde@sentry.oci.pikokr.dev/3',
+    integrations: [new SentryRRWeb()]
   });
 };
