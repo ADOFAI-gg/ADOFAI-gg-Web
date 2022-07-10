4b21f8eaa3262cd67ea4bc2c442de5a7e2f7be4c
ppapman <48434316+ppapman1@users.noreply.github.com>
2022-07-10 17:00:31 +0900

    feat(RecentLevelsSection): add level list

--- src/components/organisms/home/RecentLevelsSection.svelte
+++ src/components/organisms/home/RecentLevelsSection.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
   import MainSectionTitle from '@/components/atoms/MainSectionTitle.svelte';
   import type { Level } from '@/types';
+  import LevelListItem from '../../organisms/levels/LevelListItem.svelte';
 
   export let levels: Level[];
 </script>
 
 <MainSectionTitle title="HOME_RECENT_LEVELS" moreLink="/levels" />
 
-<!-- <pre>{JSON.stringify(levels, null, 2)}</pre> -->
+{#each levels as level}
+  <LevelListItem {level} />
+{/each}
+
+<pre>{JSON.stringify(levels, null, 2)}</pre>
