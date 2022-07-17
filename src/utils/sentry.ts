7598cf0c267a630a0e8f68cccbe44f8cd2f4fa7e
pikokr <parangee9706@gmail.com>
2022-07-17 13:17:08 +0900

    chore: disable rrweb if not in secure context

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -4,7 +4,7 @@ import SentryRRWeb from '@sentry/rrweb';
 export const setupSentry = () => {
   Sentry.init({
     dsn: 'https://2ecc5abf3f814edd8a769cb770f26dde@sentry.oci.pikokr.dev/3',
-    integrations: [new SentryRRWeb()],
+    integrations: window.isSecureContext ? [new SentryRRWeb()] : [],
     beforeSend(event) {
       if (event.exception) {
         Sentry.showReportDialog({ eventId: event.event_id });
