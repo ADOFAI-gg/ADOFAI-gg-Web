5a63facb0e375de4fd3fc2731699e84727af2eba
pikokr <parangee9706@gmail.com>
2022-07-17 12:57:46 +0900

    chore: show feedback window on error

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -4,6 +4,13 @@ import SentryRRWeb from '@sentry/rrweb';
 export const setupSentry = () => {
   Sentry.init({
     dsn: 'https://2ecc5abf3f814edd8a769cb770f26dde@sentry.oci.pikokr.dev/3',
-    integrations: [new SentryRRWeb()]
+    integrations: [new SentryRRWeb()],
+    beforeSend(event) {
+      if (event.exception) {
+        Sentry.showReportDialog({ eventId: event.event_id });
+      }
+
+      return event;
+    }
   });
 };
