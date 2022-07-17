3adbf82e4202cab8b17450fc638eb4f70f4b8abc
pikokr <parangee9706@gmail.com>
2022-07-17 14:49:08 +0900

    chore: uninstall rrweb

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,10 +1,12 @@
 import * as Sentry from '@sentry/browser';
-import SentryRRWeb from '@sentry/rrweb';
+import { CaptureConsole } from '@sentry/integrations';
 
 export const setupSentry = () => {
+  const integrations = [new CaptureConsole()];
+
   Sentry.init({
     dsn: 'https://2ecc5abf3f814edd8a769cb770f26dde@sentry.oci.pikokr.dev/3',
-    integrations: window.isSecureContext ? [new SentryRRWeb()] : [],
+    integrations,
     beforeSend(event) {
       if (event.exception) {
         Sentry.showReportDialog({ eventId: event.event_id });
