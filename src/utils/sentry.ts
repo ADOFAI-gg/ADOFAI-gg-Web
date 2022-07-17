6da74f1194ea71e3d2be3b53687ee5b5c514dd54
pikokr <parangee9706@gmail.com>
2022-07-17 18:43:51 +0900

    chore: remove capture console integration

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,12 +1,10 @@
 import * as Sentry from '@sentry/browser';
-import { CaptureConsole } from '@sentry/integrations';
+import { ExtraErrorData } from '@sentry/integrations';
 
 export const setupSentry = () => {
-  const integrations = [new CaptureConsole()];
-
   Sentry.init({
     dsn: 'https://2ecc5abf3f814edd8a769cb770f26dde@sentry.oci.pikokr.dev/3',
-    integrations,
+    integrations: [new ExtraErrorData()],
     beforeSend(event) {
       if (event.exception) {
         Sentry.showReportDialog({ eventId: event.event_id });
