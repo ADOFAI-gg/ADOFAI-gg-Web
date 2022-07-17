ee831ba3c2cac602e939dc4ef984379314c2f794
pikokr <parangee9706@gmail.com>
2022-07-17 13:17:33 +0900

    fix: use fade transition

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -77,8 +77,8 @@
     <div
       bind:clientHeight={contentHeight}
       class="absolute w-full top-0 left-0"
-      in:fly={{ x: 300, duration: 600, easing: quadOut }}
-      out:fly={{ x: -100, duration: 600, easing: quadOut }}
+      in:fade={{ duration: 300, easing: quadOut }}
+      out:fade={{ duration: 300, easing: quadOut }}
     >
       <slot />
     </div>
