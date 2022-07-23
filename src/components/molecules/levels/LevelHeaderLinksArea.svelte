191ed8f42649ac886f53ba31b7a6517c76ab53e5
pikokr <parangee9706@gmail.com>
2022-07-23 20:22:48 +0900

    feat: open level in adofai button

--- src/components/molecules/levels/LevelHeaderLinksArea.svelte
+++ src/components/molecules/levels/LevelHeaderLinksArea.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
   import Icon from '@/components/atoms/Icon.svelte';
+  import Tooltip from '@/components/atoms/Tooltip.svelte';
+  import Translation from '@/components/utils/Translation.svelte';
   import type { Level } from '@/types';
 
   export let level: Level;
 </script>
 
 <div class="flex gap-[18px]">
+  <Tooltip placement="bottom-end">
+    <a slot="button" href="adofaigg://level/{level.id}">
+      <Icon icon="play" size={36} />
+    </a>
+    <div class="p-2 text-lg font-medium">
+      <Translation key="LEVEL_OPEN_IN_ADOFAI" />
+    </div>
+  </Tooltip>
   {#if level.workshop}
     <a href={level.workshop} target="_blank" rel="noreferer"><Icon icon="steam" size={36} /></a>
   {/if}
