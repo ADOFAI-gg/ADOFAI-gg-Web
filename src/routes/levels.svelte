db03cf9ca5a4c7ac60be7daf5a31039676d22564
pikokr <parangee9706@gmail.com>
2022-07-31 14:38:45 +0900

    chore: enable footer on level list page

--- src/routes/levels.svelte
+++ src/routes/levels.svelte
@@ -1,13 +1,3 @@
-<script context="module" lang="ts">
-  export const load: Load = () => {
-    return {
-      stuff: {
-        hideFooter: true
-      }
-    };
-  };
-</script>
-
 <script lang="ts">
   import LevelList from '@/components/templates/LevelList.svelte';
   import type { Load } from '@sveltejs/kit';
