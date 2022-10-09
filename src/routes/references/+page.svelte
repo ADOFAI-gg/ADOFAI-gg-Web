0db7b3f8d0935c74ce4688130cf692941d629aff
pikokr <parangee9706@gmail.com>
2022-10-09 21:18:21 +0900

    feat: references route

---
+++ src/routes/references/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import MainSectionTitle from '@/components/atoms/MainSectionTitle.svelte';
+  import PageContainer from '@/components/atoms/PageContainer.svelte';
+</script>
+
+<div class="h-nav mt-[24px]" />
+
+<PageContainer wide>
+  <MainSectionTitle title="REFERENCES" />
+</PageContainer>
