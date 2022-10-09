5f8ee0372cb39da2af69d67d4ca35f34ca8173e5
pikokr <parangee9706@gmail.com>
2022-10-09 21:39:31 +0900

    feat: tab component

---
+++ src/components/atoms/TabGroup.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { setContext } from 'svelte';
+  import { writable } from 'svelte/store';
+
+  type T = $$Generic;
+
+  export let value: T;
+
+  let current = writable(value);
+
+  setContext('tabValue', current);
+
+  $: {
+    value = $current;
+  }
+  $: {
+    value = $current;
+  }
+</script>
+
+<div class="flex gap-[12px] w-full">
+  <slot />
+</div>
---
+++ src/components/molecules/TabItem.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import { getContext } from 'svelte';
+  import type { Writable } from 'svelte/store';
+  import Icon from '../atoms/Icon.svelte';
+  import Translation from '../utils/Translation.svelte';
+
+  type T = $$Generic;
+
+  const currentValue = getContext<Writable<T>>('tabValue');
+
+  export let value: T;
+
+  $: active = $currentValue === value;
+
+  export let title: string;
+
+  export let icon: string | undefined = undefined;
+
+  const activate = () => {
+    $currentValue = value;
+  };
+</script>
+
+<div
+  class="flex-grow cursor-pointer select-none flex-shrink-0 relative overflow-hidden {active
+    ? 'bg-darkblue/40'
+    : 'bg-darkblue/20 hover:bg-darkblue/30'} transition-all h-[42px] flex justify-center items-center gap-[10px] rounded-[8px] font-medium"
+  on:click={activate}
+>
+  {#if icon}
+    <Icon size={18} {icon} />
+  {/if}
+  <div>
+    <Translation key={title} />
+  </div>
+  <div
+    class="absolute bottom-0 h-[2px] w-full left-0 bg-blue transition-opacity {active
+      ? 'opacity-100'
+      : 'opacity-0'}"
+  />
+</div>
