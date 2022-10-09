7f8f4069904940b9c6d73a6899826574fa5cbe53
pikokr <parangee9706@gmail.com>
2022-10-09 20:50:39 +0900

    fix: translation key

--- src/routes/rankings/+page.svelte
+++ src/routes/rankings/+page.svelte
@@ -55,7 +55,7 @@
 
 <PageContainer>
   <div class="mt-[24px]">
-    <MainSectionTitle title="RANKINGS_TITLE" />
+    <MainSectionTitle title="RANKING_TITLE" />
   </div>
   {#if browser}
     <div class="md:grid flex flex-col mb-[16px] md:grid-cols-2 lg:grid-cols-3 gap-[24px]">
