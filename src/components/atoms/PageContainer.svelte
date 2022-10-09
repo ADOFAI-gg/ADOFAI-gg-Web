d44178314f578a68930a0c1600f09dd59981ed8d
파링 <parangee9706@gmail.com>
2022-10-10 00:43:30 +0900

    chore: remove padding if wide

--- src/components/atoms/PageContainer.svelte
+++ src/components/atoms/PageContainer.svelte
@@ -2,7 +2,7 @@
   export let wide = false;
 </script>
 
-<div class="px-4">
+<div class:px-4={!wide}>
   <div class={wide ? 'px-[16px] xl:px-[120px]' : 'max-w-[1100px] mx-auto'}>
     <slot />
   </div>
