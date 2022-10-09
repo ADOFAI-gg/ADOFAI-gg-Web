c892ba81b1f0a3c053c0434a5ed3d64c10a9b342
파링 <parangee9706@gmail.com>
2022-10-10 00:45:26 +0900

    feat: difficulty reference table
    Co-Authored-By: ppapman <48434316+ppapman1@users.noreply.github.com>

--- src/components/organisms/reference/DifficultyReference.svelte
+++ src/components/organisms/reference/DifficultyReference.svelte
@@ -56,4 +56,108 @@
   <div class="flex justify-center">
     <LoadingSpinner />
   </div>
+{:then data}
+  <table class="w-full min-w-[1680px] table-fixed">
+    <colgroup>
+      <col width="40" />
+      <col width="100" />
+    </colgroup>
+
+    <thead>
+      <tr>
+        <th>
+          <Translation key="REFERENCE_DIFFICULTY" />
+        </th>
+        <th>
+          <Translation key="REFERENCE_PP_RATING" />
+        </th>
+        <th>
+          <Translation key="REFERENCE_STANDARD_LEVEL_FIRST" />
+        </th>
+        <th>
+          <Translation key="REFERENCE_STANDARD_LEVEL_SECOND" />
+        </th>
+        <th>
+          <Translation key="REFERENCE_STANDARD_LEVEL_THIRD" />
+        </th>
+        <th>
+          <Translation key="REFERENCE_EASIEST_LEVEL" />
+        </th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {#each data as reference}
+        <tr>
+          <td>
+            <DifficultyIcon difficulty={reference.difficulty} size={28} />
+          </td>
+
+          <td class="col-border font-mono tracking-[-.1em] font-light">
+            {reference.ppRating}
+          </td>
+
+          {#each reference.levels as level}
+            {#if level}
+              <td class="col-border">
+                <div class="flex items-center gap-[12px]">
+                  {#if level.levelId}
+                    <div class="font-mono tracking-[-.1em] font-light opacity-40">
+                      #{level.levelId}
+                    </div>
+                  {/if}
+
+                  <a
+                    href={level.url ?? `/levels/${level.levelId}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {level.name}
+                  </a>
+                </div>
+              </td>
+            {:else}
+              <td class="col-border" />
+            {/if}
+          {/each}
+        </tr>
+      {/each}
+    </tbody>
+  </table>
 {/await}
+
+<style lang="scss">
+  table {
+    th {
+      text-align: left;
+
+      @apply font-regular text-white/60 text-md pb-[8px] whitespace-nowrap;
+    }
+
+    tbody {
+      tr {
+        @apply border-t border-white/20;
+
+        height: 52px;
+
+        vertical-align: middle;
+
+        td {
+          @apply py-2;
+        }
+
+        a {
+          @apply transition-all;
+
+          &:hover {
+            @apply underline;
+          }
+        }
+
+        td.col-border {
+          @apply border-l border-white/20 px-[12px];
+        }
+      }
+    }
+  }
+</style>
