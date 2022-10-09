315121fb706ccedd6b1aa4c32c636b54810f1fbf
파링 <parangee9706@gmail.com>
2022-10-10 00:45:57 +0900

    feat: full reference page
    Co-Authored-By: ppapman <48434316+ppapman1@users.noreply.github.com>

--- src/routes/references/+page.svelte
+++ src/routes/references/+page.svelte
@@ -10,23 +10,54 @@
   import PageContainer from '@/components/atoms/PageContainer.svelte';
   import TabGroup from '@/components/atoms/TabGroup.svelte';
   import TabItem from '@/components/molecules/TabItem.svelte';
+  import DifficultyIndication from '@/components/organisms/reference/DifficultyIndication.svelte';
+  import DifficultyReference from '@/components/organisms/reference/DifficultyReference.svelte';
   import { writable } from 'svelte/store';
 
   const currentTab = writable(ReferenceTab.Reference);
+
+  $: isReferenceTab = $currentTab === ReferenceTab.Reference;
 </script>
 
 <div class="h-nav mt-[24px]" />
 
-<PageContainer wide>
-  <MainSectionTitle title="REFERENCES" />
+<div class="table-view-header-area">
+  <PageContainer wide={isReferenceTab}>
+    <MainSectionTitle title="REFERENCES" />
 
-  <TabGroup bind:value={$currentTab}>
-    <TabItem icon="scale" title="DIFFICULTY_REFERENCE" value={ReferenceTab.Reference} />
-    <TabItem icon="question" title="DIFFICULTY_INDICATION" value={ReferenceTab.Indication} />
-  </TabGroup>
+    <TabGroup bind:value={$currentTab}>
+      <TabItem icon="scale" title="DIFFICULTY_REFERENCE" value={ReferenceTab.Reference} />
+      <TabItem icon="question" title="DIFFICULTY_INDICATION" value={ReferenceTab.Indication} />
+    </TabGroup>
+  </PageContainer>
+</div>
 
-  <div class="mt-[16px]">
+{#if $currentTab === ReferenceTab.Reference}
+  <div class="mt-[16px] table-view-container">
     <!-- Content -->
-    {$currentTab}
+    <DifficultyReference />
   </div>
-</PageContainer>
+{/if}
+{#if $currentTab === ReferenceTab.Indication}
+  <div class="mt-[16px]" />
+  <PageContainer>
+    <DifficultyIndication />
+  </PageContainer>
+{/if}
+
+<style lang="scss">
+  .table-view-header-area {
+    width: 100vw;
+    position: sticky;
+    left: 0;
+  }
+
+  .table-view-container {
+    padding-left: 24px;
+    padding-right: 24px;
+    @media screen and (min-width: 1280px) {
+      padding-left: 120px;
+      padding-right: 120px;
+    }
+  }
+</style>
