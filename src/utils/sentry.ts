0ca7690d696279953e58628d323b8c4b909898da
파링 <parangee9706@gmail.com>
2022-10-10 00:48:44 +0900

    chore: customize sentry report dialog(WIP)
    Co-Authored-By: ppapman <48434316+ppapman1@users.noreply.github.com>

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -11,7 +11,20 @@ export const setupSentry = () => {
         if (['EvalError', 'SyntaxError'].includes(event.exception.values?.[0].type as string)) {
           return null;
         }
-        Sentry.showReportDialog({ eventId: event.event_id });
+        Sentry.showReportDialog({
+          eventId: event.event_id
+          // title: '',
+          // subtitle: '',
+          // subtitle2: '',
+          // labelName: '',
+          // labelEmail: '',
+          // labelComments: '',
+          // labelClose: '',
+          // labelSubmit: '',
+          // errorGeneric: '',
+          // errorFormEntry: '',
+          // successMessage: '',
+        });
       }
 
       return event;
