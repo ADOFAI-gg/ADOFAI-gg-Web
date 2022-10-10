a87779b9c4e5cc9fcffd4a64e96755caad21b34d
pikokr <parangee9706@gmail.com>
2022-10-10 13:25:50 +0900

    chore: disable selection for logos

--- src/components/atoms/HomeLogoImage.svelte
+++ src/components/atoms/HomeLogoImage.svelte
@@ -4,8 +4,8 @@
   import Image from './Image.svelte';
 </script>
 
-<div style="aspect-ratio: 7/1;" class="relative w-full">
+<div style="aspect-ratio: 7/1;" class="relative w-full select-none">
   <div class="absolute left-0 top-0 w-full h-full">
-    <Image src={Asset.url('images/logo.svg  ')} alt="" width="100%" height="100%" />
+    <Image src={Asset.url('images/logo.svg')} alt="Logo" width="100%" height="100%" />
   </div>
 </div>
