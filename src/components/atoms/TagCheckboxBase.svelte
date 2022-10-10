1b9b5165e0298200534ff5b4cdf2ae11d1cd982c
pikokr <parangee9706@gmail.com>
2022-10-10 14:12:16 +0900

    fix: opacity issue

--- src/components/atoms/TagCheckboxBase.svelte
+++ src/components/atoms/TagCheckboxBase.svelte
@@ -37,10 +37,9 @@
 
 <style lang="scss">
   .exclude {
+    @apply opacity-100;
+
     /* filter: invert(30%) sepia(57%) saturate(6528%) hue-rotate(333deg) brightness(99%) contrast(94%); */
-    color: #d65a46;
-  }
-  .exclude-color {
     color: #d65a46;
   }
   .include {
