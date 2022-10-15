bef1575e21a70f3dfb607ee2dd4165e5ee20c74a
ppapman <48434316+ppapman1@users.noreply.github.com>
2022-10-15 18:39:36 +0900

    feat(LefelHeader): add header backblur

--- src/components/organisms/levels/info/LevelHeader.svelte
+++ src/components/organisms/levels/info/LevelHeader.svelte
@@ -14,6 +14,8 @@
     class="absolute left-0 bg-center bg-no-repeat bg-cover brightness-[60%] -z-10 top-0 w-full h-full"
     style="background-image: url(https://i.ytimg.com/vi/{youtubeId}/original.jpg)"
   />
+  <div class="absolute left-0 backdrop-blur -z-10 top-0 w-full h-full" />
+
   <div class="h-nav" />
   <div class="max-w-[1100px] w-full mx-auto">
     <div class="mt-[40px] text-4xl font-bold text-shadow-5 min-h-[140px] flex items-end">
