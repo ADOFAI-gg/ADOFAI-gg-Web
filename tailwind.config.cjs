0356a3da4221d6570323c1ab8df43a6a6c6ee7b1
ppapman <48434316+ppapman1@users.noreply.github.com>
2022-10-16 22:38:19 +0900

    style: format stylesheed, move styles from tailwind config file
    Co-authored-by: CrackThrough <CrackThrough@users.noreply.github.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,15 +5,18 @@ module.exports = {
   theme: {
     extend: {
       keyframes: (theme) => ({
+        // 가져옴
         fadeIn: {
           '0%': { opacity: 0 },
           '100%': { opacity: 1 }
         }
       }),
       animation: {
+        // 가져옴
         'fade-in': 'fadeIn 0.2s linear'
       },
       colors: {
+        // 가져옴
         yellow: '#FFE76E',
         red: '#F54F51',
         blue: '#4D93FC',
@@ -25,6 +28,7 @@ module.exports = {
         white: '#FFFFFF'
       },
       fontSize: {
+        // 가져옴
         xs: '11px',
         md: '14px',
         lg: '16px',
@@ -34,22 +38,26 @@ module.exports = {
         '4xl': '36px'
       },
       fontFamily: {
+        // 가져옴
         sans: ['"Quicksand", "M PLUS Rounded 1c", "NanumSquareRound", sans-serif'],
         mono: ['"Roboto Mono"', 'monospace']
       },
       fontWeight: {
+        // 가져옴
         light: '300',
         regular: '400',
         medium: '500',
         bold: '700'
       },
       letterSpacing: {
+        // 가져옴
         normal: '-0.011em'
       },
       height: {
         nav: '56px'
       },
       dropShadow: {
+        // 가져옴
         6: '0px 0px 6px #000000',
         5: '0px 0px 5px 0px rgba(0, 0, 0, 0.75)'
       }
