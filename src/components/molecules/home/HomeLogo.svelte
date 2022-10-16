6a254c577823b7ddfd1fbac7bcbf5bad52481f9a
ppapman <48434316+ppapman1@users.noreply.github.com>
2022-10-16 15:32:08 +0900

    chore(HomeLogo): add hover/focus underline

--- src/components/molecules/home/HomeLogo.svelte
+++ src/components/molecules/home/HomeLogo.svelte
@@ -12,7 +12,7 @@
         href="https://docs.google.com/spreadsheets/d/1PzLHfWmVWJHrBGnNSsLTsdH0ibdk0hB4MpKHET1nkpU/edit#gid=1848316468"
         rel="noreferrer"
         target="_blank"
-        class="text-yellow"
+        class="text-yellow hover:underline focus:underline"
       >
         Unofficial ADOFAI Forum?
       </a>
