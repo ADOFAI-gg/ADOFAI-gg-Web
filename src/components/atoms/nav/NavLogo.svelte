562b456f01b8b727949416ea2a5cb4d63c2199e0
ppapman <48434316+ppapman1@users.noreply.github.com>
2022-10-17 01:02:45 +0900

    style: format NavLogo
    Co-authored-by: CrackThrough <CrackThrough@users.noreply.github.com>

--- src/components/atoms/nav/NavLogo.svelte
+++ src/components/atoms/nav/NavLogo.svelte
@@ -4,6 +4,12 @@
   import Image from '@atoms/asset/Image.svelte';
 </script>
 
-<a aria-label="ADOFAI.gg" class="select-none" href="/">
+<a aria-label="ADOFAI.gg" href="/">
   <Image src={Asset.url('images/logo.svg')} alt="ADOFAI.gg Logo" width="139" height="20" />
 </a>
+
+<style lang="scss">
+  a {
+    user-select: none;
+  }
+</style>
