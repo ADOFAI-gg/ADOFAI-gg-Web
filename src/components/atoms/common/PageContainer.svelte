aa9e17ede3dc3207c8b145a7e3edb8210045a0ec
ppapman <48434316+ppapman1@users.noreply.github.com>
2022-10-16 23:32:32 +0900

    style: format PageContainer

--- src/components/atoms/common/PageContainer.svelte
+++ src/components/atoms/common/PageContainer.svelte
@@ -2,8 +2,32 @@
   export let wide = false;
 </script>
 
-<div class:px-4={!wide}>
-  <div class={wide ? 'px-[16px] xl:px-[120px]' : 'max-w-[1100px] mx-auto'}>
+<div data-wide={wide} class="page-container">
+  <div>
     <slot />
   </div>
 </div>
+
+<style lang="scss">
+  .page-container {
+    &[data-wide='false'] {
+      padding: 0 4px;
+
+      & > div {
+        max-width: 1100px;
+        margin-right: auto;
+        margin-left: auto;
+      }
+    }
+
+    &[data-wide='true'] {
+      & > div {
+        padding: 0 16px;
+
+        @media (min-width: 1280px) {
+          padding: 0 120px;
+        }
+      }
+    }
+  }
+</style>
