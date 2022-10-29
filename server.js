b87732968a23054465058749b6ff7091dc114bc8
Pariring <68010770+pikokr@users.noreply.github.com>
2022-10-30 00:44:01 +0900

    perf: add cache-control headers

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ app.get('/healthcheck', (_req, res) => {
 
 app.use(compression());
 
+app.use((req, res, next) => {
+  if (req.path.startsWith('/assets/difficultyIcons')) {
+    res.header('cache-control', 'max-age=1209600');
+  } else if (req.path.startsWith('/assets/icons') || req.path.startsWith('/assets/tagIcons')) {
+    res.header('cache-control', 'max-age=2592000');
+  } else if (req.path === '/background.webp' || req.path === '/assets/images/logo.svg') {
+    res.header('cache-control', 'max-age=31536000');
+  }
+
+  next();
+});
+
 app.use(handler);
 
 app.listen(3000, () => {
