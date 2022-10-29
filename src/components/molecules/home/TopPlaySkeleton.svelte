9193b1c3d70d37436ccc01c020e744a08419e359
ppapman <48434316+ppapman1@users.noreply.github.com>
2022-10-29 22:47:52 +0900

    style: format TopPlaySkeleton

--- src/components/molecules/home/TopPlaySkeleton.svelte
+++ src/components/molecules/home/TopPlaySkeleton.svelte
@@ -1,10 +1,65 @@
-<div
-  class="flex flex-col gap-[15px] items-center opacity-40 last:md:col-span-2 last:md:px-[25%] last:lg:p-0 last:lg:col-span-1"
->
-  <div class="w-full aspect-video bg-white bg-repeat-round animate-pulse rounded-[5px]" />
-  <div class="flex flex-col gap-[4px] w-full items-center">
-    <div class="bg-white w-[30%] h-8 rounded-[5px] animate-pulse" />
-    <div class="bg-white w-[50%] h-6 rounded-[5px] animate-pulse" />
-    <div class="bg-white w-[60%] h-6 rounded-[5px] animate-pulse" />
+<div class="skeleton-container">
+  <div class="video" />
+
+  <div class="text-container">
+    <div class="text" style="width: 30%;" />
+    <div class="text" style="width: 50%;" />
+    <div class="text" style="width: 60%;" />
   </div>
 </div>
+
+<style lang="scss">
+  .skeleton-container {
+    $pulse: pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    align-items: center;
+    opacity: 0.4;
+
+    .video {
+      width: 100%;
+      border-radius: 5px;
+      background-color: white;
+      background-repeat: round;
+      animation: $pulse;
+      aspect-ratio: 16 / 9;
+    }
+
+    .text-container {
+      display: flex;
+      flex-direction: column;
+      gap: 4px;
+      align-items: center;
+      width: 100%;
+
+      .text {
+        height: 32px;
+        border-radius: 5px;
+        background-color: white;
+        animation: $pulse;
+      }
+    }
+
+    @media (min-width: 768px) {
+      &:last-child {
+        grid-column: span 2 / span 2;
+        margin: 0 25%;
+      }
+    }
+
+    @media (min-width: 1024px) {
+      &:last-child {
+        grid-column: span 1 / span 1;
+        margin: 0;
+      }
+    }
+  }
+
+  @keyframes pulse {
+    50% {
+      opacity: 0.5;
+    }
+  }
+</style>
