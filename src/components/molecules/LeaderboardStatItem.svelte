bc88f94c89e37c473c41c20c13c60d118cba7641
ppapman <48434316+ppapman1@users.noreply.github.com>
2022-10-30 00:32:31 +0900

    style: format LeaderboardStatItem

--- src/components/molecules/LeaderboardStatItem.svelte
+++ src/components/molecules/LeaderboardStatItem.svelte
@@ -6,16 +6,42 @@
   export let postfix = '';
 </script>
 
-<div class="flex gap-[4px] items-center">
-  <div class="opacity-60">
+<div class="stat">
+  <div class="icon">
     <Icon {icon} size={16} alt="{icon} icon" />
   </div>
-  <div class="opacity-60 text-lg font-medium">
+
+  <div class="content">
     <slot />
   </div>
+
   {#if postfix}
-    <div class="text-lg font-regular opacity-40">
+    <div class="postfix">
       {postfix}
     </div>
   {/if}
 </div>
+
+<style lang="scss">
+  .stat {
+    display: flex;
+    gap: 4px;
+    align-items: center;
+
+    .icon {
+      opacity: 0.6;
+    }
+
+    .content {
+      font-weight: 500;
+      font-size: 16px;
+      opacity: 0.6;
+    }
+
+    .postfix {
+      font-weight: 400;
+      font-size: 16px;
+      opacity: 0.4;
+    }
+  }
+</style>
