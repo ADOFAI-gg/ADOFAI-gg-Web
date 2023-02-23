e9e6c64bc528815e965c775b529275216ccdf5ed
Pariring <paring@pikokr.dev>
2023-02-23 23:33:35 +0900

    feat: raw text replacer

--- src/components/molecules/home/HomeLogo.svelte
+++ src/components/molecules/home/HomeLogo.svelte
@@ -1,22 +1,27 @@
 <script lang="ts">
   import HomeLogoImage from '@atoms/home/HomeLogoImage.svelte';
   import Translation from '@/components/utils/Translation.svelte';
+
+  const forumLinkReplacer = (html: string) => {
+    return html.replace(
+      /<forumLink>(.+?)<\/forumLink>/g,
+      `<a
+        class="forum-link"
+        href="https://docs.google.com/spreadsheets/d/1PzLHfWmVWJHrBGnNSsLTsdH0ibdk0hB4MpKHET1nkpU/edit#gid=1848316468"
+        rel="noreferrer"
+        target="_blank"
+      >
+        $1
+      </a>`
+    );
+  };
 </script>
 
 <div class="home-logo">
   <HomeLogoImage />
 
   <span class="description">
-    <Translation key="HOME_HERO_DESCRIPTION">
-      <a
-        data-key="forumLink"
-        href="https://docs.google.com/spreadsheets/d/1PzLHfWmVWJHrBGnNSsLTsdH0ibdk0hB4MpKHET1nkpU/edit#gid=1848316468"
-        rel="noreferrer"
-        target="_blank"
-      >
-        Unofficial ADOFAI Forum?
-      </a>
-    </Translation>
+    <Translation key="HOME_HERO_DESCRIPTION" htmlReplacer={forumLinkReplacer} />
   </span>
 </div>
 
@@ -32,7 +37,7 @@
       font-weight: 500;
       font-size: 24px;
 
-      a {
+      :global(a.forum-link) {
         color: rgba(var(--color-yellow), 1);
 
         &:hover,
