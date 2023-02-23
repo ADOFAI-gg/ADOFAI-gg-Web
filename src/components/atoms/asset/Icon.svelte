d33d7a3ac429487c654e1f90dae7c0ee8559539e
Pariring <paring@pikokr.dev>
2023-02-23 22:07:08 +0900

    feat: glob import icons

--- src/components/atoms/asset/Icon.svelte
+++ src/components/atoms/asset/Icon.svelte
@@ -1,67 +1,62 @@
 <script lang="ts" context="module">
-  const cache = new Map<string, Promise<SVGElement>>();
+  const cache = new Map<string, SVGElement>();
 
   const parser = browser ? new DOMParser() : (null as unknown as DOMParser);
+
+  const getIconString = (id: string, namespace: string) => {
+    return svg[`../assets/${namespace}/${id}.svg`];
+  };
 </script>
 
 <script lang="ts">
   import { browser } from '$app/environment';
 
-  import { Asset } from '@/utils/assets';
-  import axios from 'axios';
+  import { svg } from '@/utils/assets';
 
   export let icon: string;
   export let namespace = 'icons';
   export let size = 24;
   export let style = 'color: white;';
 
-  const load = async (id: string) => {
+  const load = (id: string) => {
     if (!browser) return null as unknown as SVGElement;
 
-    const src = Asset.icon(id, namespace);
+    const cacheKey = `${namespace}:${id}`;
 
-    if (cache.has(src)) return cache.get(src) as Promise<SVGElement>;
+    if (cache.has(cacheKey)) return cache.get(cacheKey) as SVGElement;
 
-    const promise = axios.get(src).then((x) => {
-      const svg: SVGElement = parser
-        .parseFromString(x.data, 'image/svg+xml')
-        .querySelector('svg') as SVGElement;
-      svg.querySelectorAll('*')?.forEach((i) => {
-        if (i.hasAttribute('fill')) i.setAttribute('fill', 'currentColor');
+    const icon = getIconString(id, namespace);
 
-        if (i.hasAttribute('stroke')) i.setAttribute('stroke', 'currentColor');
-      });
-      svg.setAttribute('aria-label', alt);
+    // TODO: preprocess at assets repository
+    const svg: SVGElement = parser
+      .parseFromString(icon, 'image/svg+xml')
+      .querySelector('svg') as SVGElement;
+    svg.querySelectorAll('*')?.forEach((i) => {
+      if (i.hasAttribute('fill')) i.setAttribute('fill', 'currentColor');
 
-      return svg;
+      if (i.hasAttribute('stroke')) i.setAttribute('stroke', 'currentColor');
     });
+    svg.setAttribute('aria-label', alt);
 
-    cache.set(src, promise);
+    cache.set(cacheKey, svg);
 
-    return promise;
+    return svg;
   };
 
   let container: HTMLDivElement;
 
-  export let disableFade = false;
   export let alt: string;
 
-  $: promise = load(icon);
+  $: instance = load(icon);
 
   $: {
-    if (promise && container) {
-      promise.then((data) => {
-        if (!container) return;
-        if (!disableFade) {
-          container.classList.add('loaded');
-        }
-        const svg = data.cloneNode(true) as SVGElement;
+    if (instance && container) {
+      const svg = instance.cloneNode(true) as SVGElement;
 
-        svg.setAttribute('width', `${size}`);
-        svg.setAttribute('height', `${size}`);
+      svg.setAttribute('width', `${size}`);
+      svg.setAttribute('height', `${size}`);
 
-        container.replaceChildren(svg);
-      });
+      container.replaceChildren(svg);
     }
   }
 </script>
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import * as path from 'path';
 import glob from 'vite-plugin-glob';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+
 export default defineConfig({
   resolve: {
     alias: {
