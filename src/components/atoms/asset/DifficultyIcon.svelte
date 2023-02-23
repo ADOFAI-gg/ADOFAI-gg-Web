d4416f23f0a02ab0188755ebbf190b506e2e2f76
Pariring <paring@pikokr.dev>
2023-02-23 22:13:04 +0900

    feat: load difficulty icons with glob import

--- src/components/atoms/asset/DifficultyIcon.svelte
+++ src/components/atoms/asset/DifficultyIcon.svelte
@@ -1,12 +1,19 @@
+<script lang="ts" context="module">
+  const icons = import.meta.glob('../../../assets/difficultyIcons/*.svg', {
+    eager: true
+  });
+</script>
+
 <script lang="ts">
-  import { Asset } from '@/utils/assets';
   import Image from '@atoms/asset/Image.svelte';
 
   export let difficulty: number;
 
   export let size = 24;
 
-  $: url = Asset.difficultyIcon(difficulty);
+  $: url = (
+    icons[`../../../assets/difficultyIcons/${difficulty}.svg`] as { default: string } | undefined
+  )?.default;
 </script>
 
 <Image src={url} width={size} alt="{difficulty} Difficulty Icon" height={size} />
