51ace06c0048d689392c7c460f0d2eb54206b3cc
Pariring <paring@pikokr.dev>
2023-02-23 23:34:46 +0900

    chore: remove unused code

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -1,26 +1,4 @@
-import { dev } from '$app/environment';
-import axios from 'axios';
-
 export const svgIcons = import.meta.glob('../assets/{icons,tagIcons}/*.svg', {
   eager: true,
   as: 'raw'
 });
-
-const asset = (path: string) =>
-  dev
-    ? `https://raw.githubusercontent.com/ADOFAI-gg/Adofai-gg-assets/main/${path}`
-    : `/assets/${path}`;
-
-export class Asset {
-  static icon(id: string, namespace = 'icons') {
-    return asset(`${namespace}/${id}.svg`);
-  }
-
-  static url(id: string) {
-    return asset(id);
-  }
-
-  static async loadJSON<T = Record<string, string>>(key: string) {
-    return (await axios.get<T>(Asset.url(key))).data;
-  }
-}
