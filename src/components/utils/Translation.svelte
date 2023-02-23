84f349998806380ced305e667d494f5414d3d87f
Pariring <paring@pikokr.dev>
2023-02-23 23:33:35 +0900

    feat: raw text replacer

--- src/components/utils/Translation.svelte
+++ src/components/utils/Translation.svelte
@@ -1,97 +1,39 @@
 <script context="module" lang="ts">
-  const tagRegex = /<(.+)>(.+?)<\/\1>/g;
+  const escape = (str: string) =>
+    str
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#039;');
 </script>
 
 <script lang="ts">
   import { langData, currentLang, fallbackLang } from '@/utils/i18n';
-  import TranslationComponentPortal from './TranslationComponentPortal.svelte';
 
   export let key: string;
   export let params: Record<string, string> = {};
 
-  let componentsContainer: HTMLDivElement;
+  export let htmlReplacer: (value: string) => string = (v) => v;
 
-  $: components = (() => {
-    if (componentsContainer) {
-      const res: Record<string, HTMLElement> = {};
-      for (const node of Array.from(componentsContainer.children)) {
-        const key = (node as HTMLElement).dataset.key;
-        if (key) {
-          res[key] = node as HTMLElement;
-        }
-      }
-      return res;
-    }
-
-    return {} as Record<string, HTMLElement>;
-  })();
-
-  type TranslationChunk =
-    | {
-        type: 'text';
-        value: string;
-      }
-    | {
-        type: 'component';
-        tag: string;
-        value: string;
-      };
-
-  $: value = (() => {
-    const keyResult: TranslationChunk[] = [{ type: 'text', value: key }];
+  $: htmlValue = (() => {
     const l = $currentLang;
-    if (!l) return keyResult;
-    const lang = langData[l];
-    if (!lang) return keyResult;
+    const lang = langData[l] ?? langData[fallbackLang];
+
+    if (!lang) return escape(key);
+
     let k = lang[key] ?? langData[fallbackLang][key];
-    if (!k) return keyResult;
-    const matches = k.matchAll(tagRegex);
-    const result: TranslationChunk[] = [];
-    let lastIndex = 0;
-    let matched = false;
-    let lastLength = 0;
-    for (const match of matches) {
-      lastLength = match[0].length;
-      matched = true;
-      if (match.input && lastIndex !== match.index) {
-        result.push({
-          type: 'text',
-          value: match.input.slice(lastIndex, match.index)
-        });
-      }
-      result.push({ tag: match[1], value: match[2], type: 'component' });
-      if (match.index) {
-        lastIndex = match.index;
-      }
+
+    if (!k) return key;
+
+    for (const paramId in params) {
+      k = k.split(`{${paramId}}`).join(escape(params[paramId])); // replace all
     }
-    if (!matched) {
-      result.push({ type: 'text', value: k });
-    } else {
-      result.push({ type: 'text', value: k.slice(lastIndex + lastLength) });
-    }
-    for (const chunk of result) {
-      if (chunk.type === 'text') {
-        for (const key in params) {
-          chunk.value = chunk.value.split(`{${key}}`).join(params[key]);
-        }
-      }
-    }
-    return result;
+
+    k = htmlReplacer(k);
+
+    return k;
   })();
 </script>
 
-{#each value as chunk, i (i)}
-  {#if chunk.type === 'text'}
-    {chunk.value}
-  {:else if chunk.type === 'component'}
-    {#if components[chunk.tag]}
-      <TranslationComponentPortal component={components[chunk.tag]} text={chunk.value} />
-    {:else}
-      <div>{chunk.value}</div>
-    {/if}
-  {/if}
-{/each}
-
-<div style="display: none;" bind:this={componentsContainer}>
-  <slot />
-</div>
+{@html htmlValue}
