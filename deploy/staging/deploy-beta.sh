9791563e66a6d49aa930dfc263c27a41f5a2b50e
Pariring <paring@pikokr.dev>
2023-02-26 14:38:21 +0900

    feat: beta kubernetes config

---
+++ deploy/staging/deploy-beta.sh
@@ -0,0 +1,3 @@
+curl --insecure -H "Authorization: Bearer $API_TOKEN" $API_ENDPOINT/apis/apps/v1/namespaces/adofai-gg/deployments/frontend-beta \
+	-X PATCH -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" \
+	--data '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubrnetes.io/restartedAt":"'"$(date +%Y-%m-%dT%T%z)"'"}}}}}' > /dev/null
