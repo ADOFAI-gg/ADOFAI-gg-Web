d63295d6c2e79d60992fe5423d8a8715d2dfb966
파링 <paring@adofai.gg>
2023-02-28 14:53:20 +0900

    feat: sentry rrweb

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,11 +1,12 @@
 import * as Sentry from '@sentry/browser';
 import { ExtraErrorData } from '@sentry/integrations';
 import { Integrations } from '@sentry/tracing';
+import SentryRRWeb from '@sentry/rrweb';
 
 export const setupSentry = () => {
   Sentry.init({
     dsn: 'https://2ecc5abf3f814edd8a769cb770f26dde@sentry.oci.pikokr.dev/3',
-    integrations: [new ExtraErrorData(), new Integrations.BrowserTracing()],
+    integrations: [new ExtraErrorData(), new Integrations.BrowserTracing(), new SentryRRWeb()],
     beforeSend(event) {
       if (event.exception) {
         if (['EvalError', 'SyntaxError'].includes(event.exception.values?.[0].type as string)) {
