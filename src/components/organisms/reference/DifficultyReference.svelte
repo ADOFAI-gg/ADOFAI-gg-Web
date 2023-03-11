6092c8a9cb7529f80f51926dcb4bee0dc6ad92ce
파링 <paring@pikokr.dev>
2023-03-11 10:04:56 +0900

    perf: optimize difficulty reference

--- src/components/organisms/reference/DifficultyReference.svelte
+++ src/components/organisms/reference/DifficultyReference.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
-  import { browser } from '$app/environment';
   import LoadingSpinner from '@atoms/common/LoadingSpinner.svelte';
-  import type { DifficultyReference, DifficultyReferenceLevel, RawPPRatings } from '@/types';
+  import type { DifficultyReference, DifficultyReferenceLevel } from '@/types';
   import DifficultyIcon from '@atoms/asset/DifficultyIcon.svelte';
   import Translation from '@/components/utils/Translation.svelte';
   import Table from '@atoms/table/Table.svelte';
   import { default as Cell } from '@atoms/table/TableCell.svelte';
+  import levels from '@/assets/data/difficultyReferences.json';
+  import ratings from '@/assets/data/ppRatings.json';
 
-  const loadData = async (): Promise<DifficultyReference[]> => {
-    if (!browser) return [];
-
-    const levels = (await import('@/assets/data/difficultyReferences.json')).default;
-    const ratings = (await import('@/assets/data/ppRatings.json')).default as RawPPRatings[];
-
+  const loadData = (): DifficultyReference[] => {
     const referenceMap = new Map<number, DifficultyReference>();
 
     for (const rating of ratings) {
@@ -45,91 +41,80 @@
     return result;
   };
 
-  const loadPromise = loadData();
+  const data = loadData();
 </script>
 
 <div class="reference-table">
-  {#await loadPromise}
-    <div class="loading-container">
-      <LoadingSpinner />
-    </div>
-  {:then data}
-    <Table class="reference-table">
-      <colgroup>
-        <col width="40" />
-        <col width="100" />
-      </colgroup>
+  <Table class="reference-table">
+    <colgroup>
+      <col width="40" />
+      <col width="100" />
+    </colgroup>
 
-      <thead>
+    <thead>
+      <tr>
+        <th>
+          <Translation key="REFERENCE_DIFFICULTY" />
+        </th>
+        <th>
+          <Translation key="REFERENCE_PP_RATING" />
+        </th>
+        <th>
+          <Translation key="REFERENCE_STANDARD_LEVEL_FIRST" />
+        </th>
+        <th>
+          <Translation key="REFERENCE_STANDARD_LEVEL_SECOND" />
+        </th>
+        <th>
+          <Translation key="REFERENCE_STANDARD_LEVEL_THIRD" />
+        </th>
+        <th>
+          <Translation key="REFERENCE_EASIEST_LEVEL" />
+        </th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {#each data as reference}
         <tr>
-          <th>
-            <Translation key="REFERENCE_DIFFICULTY" />
-          </th>
-          <th>
-            <Translation key="REFERENCE_PP_RATING" />
-          </th>
-          <th>
-            <Translation key="REFERENCE_STANDARD_LEVEL_FIRST" />
-          </th>
-          <th>
-            <Translation key="REFERENCE_STANDARD_LEVEL_SECOND" />
-          </th>
-          <th>
-            <Translation key="REFERENCE_STANDARD_LEVEL_THIRD" />
-          </th>
-          <th>
-            <Translation key="REFERENCE_EASIEST_LEVEL" />
-          </th>
+          <Cell>
+            <DifficultyIcon difficulty={reference.difficulty} size={28} />
+          </Cell>
+
+          <Cell class="reference-table__pp-rating-cell" border>
+            {reference.ppRating}
+          </Cell>
+
+          {#each reference.levels as level}
+            {#if level}
+              <Cell leftSideBorder>
+                <div class="reference-table__level-cell-content">
+                  {#if level.levelId}
+                    <div class="reference-table__level-id-text">
+                      #{level.levelId}
+                    </div>
+                  {/if}
+
+                  <a
+                    href={level.url ?? `/levels/${level.levelId}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {level.name}
+                  </a>
+                </div>
+              </Cell>
+            {:else}
+              <Cell leftSideBorder />
+            {/if}
+          {/each}
         </tr>
-      </thead>
-
-      <tbody>
-        {#each data as reference}
-          <tr>
-            <Cell>
-              <DifficultyIcon difficulty={reference.difficulty} size={28} />
-            </Cell>
-
-            <Cell class="reference-table__pp-rating-cell" border>
-              {reference.ppRating}
-            </Cell>
-
-            {#each reference.levels as level}
-              {#if level}
-                <Cell leftSideBorder>
-                  <div class="reference-table__level-cell-content">
-                    {#if level.levelId}
-                      <div class="reference-table__level-id-text">
-                        #{level.levelId}
-                      </div>
-                    {/if}
-
-                    <a
-                      href={level.url ?? `/levels/${level.levelId}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {level.name}
-                    </a>
-                  </div>
-                </Cell>
-              {:else}
-                <Cell leftSideBorder />
-              {/if}
-            {/each}
-          </tr>
-        {/each}
-      </tbody>
-    </Table>
-  {/await}
+      {/each}
+    </tbody>
+  </Table>
 </div>
 
 <style lang="scss">
-  .loading-container {
-    display: flex;
-    justify-content: cetner;
-  }
-
   .reference-table {
     :global(.reference-table) {
       width: 100%;
