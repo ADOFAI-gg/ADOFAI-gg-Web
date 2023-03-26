cb3ae73b41d88b25e8f4e80dbeb412f164affad9
파링 <paring@pikokr.dev>
2023-03-26 16:48:40 +0900

    feat: mobile view for level leaderboard

--- src/components/organisms/levels/info/LevelLeaderboardItem.svelte
+++ src/components/organisms/levels/info/LevelLeaderboardItem.svelte
@@ -2,51 +2,62 @@
   import Icon from '@atoms/asset/Icon.svelte';
   import LeaderboardStatItem from '@molecules/LeaderboardStatItem.svelte';
   import type { PlayLog } from '@/types';
+  import Translation from '@/components/utils/Translation.svelte';
 
   export let play: PlayLog;
   export let index: number;
 </script>
 
 <article>
-  <div class="left-side">
-    <div class="left-side__rank">#{index + 1}</div>
-
-    <div class="left-side__player">
-      <div class="left-side__player-name">{play.player.name}</div>
-    </div>
+  <div class="rank">
+    #{index + 1}
   </div>
 
-  <div class="right-side">
-    <div style="width: 128px;">
-      <LeaderboardStatItem icon="fire" postfix="PP">
-        {play.playPoint.toFixed(3)}
-      </LeaderboardStatItem>
+  <div class="details">
+    <div class="left-side">
+      <div class="left-side__player">
+        <div class="left-side__player-name">{play.player.name}</div>
+        {#if play.description}
+          <div class="left-side__player-description">{play.description}</div>
+        {/if}
+      </div>
     </div>
 
-    <div style="width: 96px;">
-      <LeaderboardStatItem icon="accuracy" postfix="%">
-        {Number(play.xAccuracy.toFixed(3))}
-      </LeaderboardStatItem>
+    <div class="right-side">
+      <div style="width: 128px;" class="right-side__item">
+        <LeaderboardStatItem icon="fire" postfix="PP">
+          {play.playPoint.toFixed(3)}
+        </LeaderboardStatItem>
+      </div>
+
+      <div style="width: 84px;" class="right-side__item">
+        <LeaderboardStatItem icon="accuracy" postfix="%">
+          {Number(play.xAccuracy.toFixed(3))}
+        </LeaderboardStatItem>
+      </div>
+
+      <div style="width: 84px;" class="right-side__item">
+        <LeaderboardStatItem icon="speed" postfix="x">
+          {play.speed / 100}
+        </LeaderboardStatItem>
+      </div>
+
+      <div class="right-side__divider right-side__item" />
+
+      <a href={play.url} target="_blank" class="right-side__video-link" rel="noreferrer">
+        <Icon icon="playOutlined" size={16} alt="Video Icon" />
+        <span>
+          <Translation key="LEADERBOARD_CLEAR_VIDEO" />
+        </span>
+      </a>
     </div>
-
-    <div style="width: 84px;">
-      <LeaderboardStatItem icon="speed" postfix="x">
-        {play.speed / 100}
-      </LeaderboardStatItem>
-    </div>
-
-    <div class="right-side__divider" />
-
-    <a href={play.url} target="_blank" class="right-side__video-link" rel="noreferrer">
-      <Icon icon="playOutlined" size={16} alt="Video Icon" />
-      <span>Clear Video</span>
-    </a>
   </div>
 </article>
 
 <style lang="scss">
   article {
     display: flex;
+    gap: 16px;
     align-items: center;
     justify-content: space-between;
     padding: 12px 16px;
@@ -59,34 +70,59 @@
     }
   }
 
+  .rank {
+    flex-shrink: 0;
+    width: 56px;
+    font-size: 24px;
+    font-family: var(--font-mono);
+    text-align: center;
+  }
+
+  .details {
+    display: flex;
+    flex-grow: 1;
+
+    @media (max-width: 768px) {
+      flex-direction: column;
+    }
+  }
+
   .left-side {
     display: flex;
-    gap: 14px;
-    align-items: center;
-
-    &__rank {
-      width: 56px;
-      font-size: 24px;
-      font-family: var(--font-mono);
-      text-align: center;
-    }
+    flex-grow: 1;
+    flex-direction: column;
+    gap: 2px;
 
     &__player {
       display: flex;
-      gap: 8px;
-      align-items: center;
+      flex-direction: column;
+      justify-content: center;
 
       &-name {
         font-weight: 500;
         font-size: 21px;
       }
+
+      &-description {
+        font-weight: 500;
+        font-size: 14px;
+        opacity: 0.6;
+      }
+    }
+
+    @media (min-width: 769px) {
+      width: 0;
     }
   }
 
   .right-side {
     display: flex;
-    gap: 12px;
+    flex-wrap: wrap;
     align-items: center;
+
+    &__item {
+      margin-right: 12px;
+    }
 
     &__divider {
       width: 1px;
@@ -98,6 +134,7 @@
       display: flex;
       gap: 6px;
       align-items: center;
+      white-space: nowrap;
       transition: font-weight 0.1s ease;
 
       &:hover,
