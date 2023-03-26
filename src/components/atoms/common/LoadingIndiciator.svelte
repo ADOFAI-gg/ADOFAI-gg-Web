ca0717adfc39a3441be2e1bd69b2b3aa96dfa4ae
파링 <paring@pikokr.dev>
2023-03-26 17:02:30 +0900

    feat: scroll to top on navigation end

--- src/components/atoms/common/LoadingIndiciator.svelte
+++ src/components/atoms/common/LoadingIndiciator.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
   import { navigating } from '$app/stores';
+  import { afterNavigate } from '$app/navigation';
   import NProgress from 'nprogress';
+
+  afterNavigate(() => {
+    const app = document.querySelector('#root');
+
+    app?.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  });
 
   $: {
     if ($navigating) {
