456a4f1d7dd6efc1686d69f1fd33f4add9a357d4
paring <paring@pikokr.dev>
2023-05-07 19:04:13 +0900

    feat: match documentation style with design

--- src/routes/docs/[...slug]/+page.svelte
+++ src/routes/docs/[...slug]/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
   import { page } from '$app/stores';
-  import 'github-markdown-css/github-markdown-dark.css';
 </script>
 
 <div class="title">
   {$page.data.metadata.title}
 </div>
+
+{#if $page.data.metadata.summary}
+  <div class="summary">{$page.data.metadata.summary}</div>
+{/if}
+
+<div class="top-divider" />
 
 <div class="markdown-body">
   <svelte:component this={$page.data.component} />
@@ -13,8 +18,22 @@
 
 <style>
   .title {
-    font-weight: 600;
-    font-size: 36px;
+    font-weight: 700;
+    font-size: 48px;
+  }
+
+  .summary {
+    margin-top: 4px;
+    font-size: 18;
+    opacity: 0.6;
+  }
+
+  .top-divider {
+    width: 100%;
+    height: 2px;
+    margin-top: 8px;
+    margin-bottom: 8px;
+    background-color: rgba(255, 255, 255, 0.2);
   }
 
   .markdown-body {
