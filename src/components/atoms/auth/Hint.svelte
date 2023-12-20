730576553247b84c03185516275a7cd307c5d43c
CrackThrough <crackthrough@gmail.com>
2023-12-20 12:34:28 +0900

    feat: add hint text

---
+++ src/components/atoms/auth/Hint.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  export let warn: boolean = false;
+</script>
+
+<div class="hint">
+  {#if warn}
+    <div class="warn-marker" />
+    <p class="warn">
+      <slot />
+    </p>
+  {:else}
+    <p class="normal">
+      <slot />
+    </p>
+  {/if}
+</div>
+
+<style lang="scss">
+  .hint {
+    display: flex;
+    flex-direction: row;
+    column-gap: 6px;
+    align-items: center;
+
+    .normal {
+      font-size: 12px;
+    }
+
+    .warn {
+      font-size: 14px;
+      color: rgb(var(--color-red));
+    }
+
+    .warn-marker {
+      width: 8px;
+      height: 8px;
+      border-radius: 100%;
+      background-color: rgb(var(--color-red));
+    }
+  }
+</style>
