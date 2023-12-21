3707005846f253aaef62281621f00f848c76906c
CrackThrough <crackthrough@gmail.com>
2023-12-21 20:35:31 +0900

    feat: show/hide password input content
    Co-authored-by: paring <me@paring.moe>

--- src/components/atoms/interaction/InputField.svelte
+++ src/components/atoms/interaction/InputField.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
   import { currentLangData, type TranslationKey } from '@/utils/i18n';
+  import Icon from '@atoms/asset/Icon.svelte';
 
   export let value: TranslationKey = '';
   export let placeholder: string = '';
+  export let passwordVisible: boolean = false;
+
+  let input: HTMLInputElement;
+  let showPassword: boolean = false;
+
+  const togglePasswordVisibility = () => {
+    showPassword = !showPassword;
+    input.type = showPassword ? 'text' : 'password';
+  };
 
   $: localizedPlaceholder = $currentLangData[placeholder];
 </script>
@@ -14,8 +24,22 @@
       class="input-field__input"
       placeholder={localizedPlaceholder}
       {value}
+      bind:this={input}
       {...$$restProps}
     />
+    {#if passwordVisible}
+      <button
+        type="button"
+        class="input-field__password-button"
+        on:click={togglePasswordVisibility}
+      >
+        {#if showPassword}
+          <Icon icon="hide" size={16} alt="Hide Password" />
+        {:else}
+          <Icon icon="show" size={16} alt="Show Password" />
+        {/if}
+      </button>
+    {/if}
   </div>
 </div>
 
@@ -32,6 +56,7 @@
     flex-grow: 1;
     align-self: stretch;
     padding: 4px 0;
+    position: relative;
 
     &__input {
       background-color: transparent;
@@ -54,6 +79,19 @@
       &::placeholder {
         color: white;
         opacity: 0.4;
+      }
+    }
+
+    &__password-button {
+      opacity: 0.4;
+      right: 0;
+      top: 50%;
+      transform: translateY(-50%);
+      position: absolute;
+      transition: opacity ease-in-out 0.1s;
+
+      &:hover {
+        opacity: 0.6;
       }
     }
   }
