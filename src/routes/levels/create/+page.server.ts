e1681f1d997416e0e678cfa5a9def0ae2c27e06f
paring <paringchan@proton.me>
2025-03-07 00:57:45 +0900

    fix: login url

--- src/routes/levels/create/+page.server.ts
+++ src/routes/levels/create/+page.server.ts
@@ -2,11 +2,11 @@ import { redirect } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 import { env } from '$env/dynamic/public'
 
-export const load: PageServerLoad = async ({ parent, cookies }) => {
+export const load: PageServerLoad = async ({ parent, cookies, url }) => {
 	const { currentUser } = await parent()
 
 	if (!currentUser) {
-		cookies.set('redirectTo', window.location.href, { domain: env.PUBLIC_COOKIE_DOMAIN, path: '/' })
+		cookies.set('redirectTo', url.toString(), { domain: env.PUBLIC_COOKIE_DOMAIN, path: '/' })
 		return redirect(302, `${env.PUBLIC_ACCOUNT_SERVICE_URL}/auth/signin`)
 	}
 }
