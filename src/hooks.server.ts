05af9c01275312190a421f22a78008137790c1e2
paring <me@paring.moe>
2025-04-14 03:09:55 +0900

    feat: rewrite internal request

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,9 @@
+import { env as pubEnv } from '$env/dynamic/public'
+import { env as privEnv } from '$env/dynamic/private'
 import type { Handle, HandleFetch } from '@sveltejs/kit'
+
+const base = pubEnv.PUBLIC_API_BASE!
+const target = privEnv.INTERNAL_API_BASE
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event)
@@ -8,5 +13,11 @@ export const handle: Handle = async ({ event, resolve }) => {
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
 	const cookie = event.request.headers.get('Cookie')
 	if (cookie) request.headers.set('Cookie', cookie)
+
+	if (target && request.url.startsWith(base)) {
+		const res = await fetch(request.url.replace(base, target), request)
+		return res
+	}
+
 	return await fetch(request)
 }
