8dd039de59f4d6a3ccdc73af287c180c864790dc
paring <me@paring.moe>
2025-04-15 12:35:13 +0900

    feat: init spotlight

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,13 +1,23 @@
 import { dev } from '$app/environment'
 import { handleErrorWithSentry } from '@sentry/sveltekit'
 import * as Sentry from '@sentry/sveltekit'
+import { env } from '$env/dynamic/public'
+import * as Spotlight from '@spotlightjs/spotlight'
 
 Sentry.init({
 	dsn: 'https://b0666c31b6c64ee6b8c89c6f461ce173@trace.afg.ink/1',
 	tracesSampleRate: 1.0,
 
-	environment: dev ? 'dev' : 'prod'
+	environment: dev ? 'dev' : env.PUBLIC_SENTRY_ENV || 'prod',
+
+	spotlight: dev
 })
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
 export const handleError = handleErrorWithSentry()
+
+if (dev) {
+	Spotlight.init({
+		injectImmediately: true
+	})
+}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,11 +4,14 @@ import { env as pubEnv } from '$env/dynamic/public'
 import { env as privEnv } from '$env/dynamic/private'
 import type { Handle, HandleFetch } from '@sveltejs/kit'
 import { dev } from '$app/environment'
+import { setupSidecar } from '@spotlightjs/spotlight/sidecar'
 
 Sentry.init({
 	dsn: 'https://b0666c31b6c64ee6b8c89c6f461ce173@trace.afg.ink/1',
 	tracesSampleRate: 1,
-	environment: dev ? 'dev' : 'prod'
+	environment: dev ? 'dev' : pubEnv.PUBLIC_SENTRY_ENV || 'prod',
+
+	spotlight: dev
 })
 
 const base = pubEnv.PUBLIC_API_BASE!
@@ -31,3 +34,7 @@ export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
 	return await fetch(request)
 }
 export const handleError = Sentry.handleErrorWithSentry()
+
+if (dev) {
+	setupSidecar()
+}
