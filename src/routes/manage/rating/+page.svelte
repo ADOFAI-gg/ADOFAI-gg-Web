e94ee5769130e04511af44742084a6cf0fa425c3
paring <me@paring.moe>
2025-04-30 17:16:49 +0900

    feat: wip level rating ui

--- src/routes/manage/rating/+page.svelte
+++ src/routes/manage/rating/+page.svelte
@@ -1,8 +1,24 @@
-<div class="level-list">wow</div>
-<div>wow</div>
+<script>
+	import LevelInfo from './level-info.svelte'
+	import RatingItem from './rating-item.svelte'
+	import Searchbar from './searchbar.svelte'
+</script>
+
+<div class="level-list">
+	<Searchbar />
+	<div class="level-list-content">
+		<RatingItem />
+	</div>
+</div>
+
+<LevelInfo />
 
 <style lang="scss">
 	.level-list {
 		width: 400px;
+
+		&-content {
+			margin-top: 16px;
+		}
 	}
 </style>
---
+++ src/routes/manage/rating/level-info.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import InfoMainSection from './info-main-section.svelte'
+</script>
+
+<!-- <div class="no-levels">
+	<Translation key="manage:no-levels" />
+</div> -->
+
+<InfoMainSection />
+
+<div class="details-section">asdf</div>
+
+<style lang="scss">
+	@use '@adofai-gg/ui/dist/stylesheets/system/colors' as *;
+
+	.no-levels {
+		text-align: center;
+		width: 0;
+		flex-grow: 1;
+		height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		opacity: 0.4;
+		user-select: none;
+	}
+
+	.details-section {
+		width: 400px;
+	}
+</style>
